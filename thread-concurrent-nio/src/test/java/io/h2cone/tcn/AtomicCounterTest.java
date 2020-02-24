08d7c91ee30685890819b69802efbeac791df54d
h2cone <tw8ape@gmail.com>
2020-02-24 11:59:58 +0800

    Add `AtomicCounter`

---
+++ thread-concurrent-nio/src/test/java/io/h2cone/tcn/AtomicCounterTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 hehuang https://github.com/h2cone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.h2cone.tcn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AtomicCounterTest {
+    private static final long wait = 3000;
+
+    private final long threads = 2;
+    private final long times = 2000000;
+    private final long excepted = threads * times;
+
+    @Test
+    public void testIncrement() throws InterruptedException {
+        AtomicCounter counter = new AtomicCounter();
+
+        startThreads(counter, () -> {
+            for (int j = 0; j < times; j++) {
+                counter.increment();
+            }
+            System.out.printf("threadName: %s, counterValue: %s\n", Thread.currentThread().getName(), counter.value());
+        });
+        Assert.assertEquals(excepted, counter.value());
+    }
+
+    private void startThreads(AtomicCounter counter, Runnable runnable) throws InterruptedException {
+        for (int i = 0; i < threads; i++) {
+            new Thread(runnable).start();
+        }
+        Thread.sleep(AtomicCounterTest.wait);
+        System.out.printf("threadName: %s, exceptedCounterValue: %s, actualCounterValue: %s\n", Thread.currentThread().getName(), excepted, counter.value());
+    }
+}
