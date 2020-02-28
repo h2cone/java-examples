91566329bc1bf3396f3f8fdab8bb9cf1c486fed3
h2cone <tw8ape@gmail.com>
2020-02-28 21:45:21 +0800

    Add `ForkBlur` and `RecursiveActionSample`

---
+++ concurrent/src/main/java/io/h2cone/concurrent/ForkBlur.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2020 hehuang https://github.com/h2cone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.h2cone.concurrent;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * Copy from https://docs.oracle.com/javase/tutorial/essential/concurrency/forkjoin.html
+ */
+public class ForkBlur extends RecursiveAction {
+    private int[] mSource;
+    private int mStart;
+    private int mLength;
+    private int[] mDestination;
+
+    // Processing window size; should be odd.
+    private int mBlurWidth = 15;
+
+    public ForkBlur(int[] src, int start, int length, int[] dst) {
+        mSource = src;
+        mStart = start;
+        mLength = length;
+        mDestination = dst;
+    }
+
+    protected void computeDirectly() {
+        int sidePixels = (mBlurWidth - 1) / 2;
+        for (int index = mStart; index < mStart + mLength; index++) {
+            // Calculate average.
+            float rt = 0, gt = 0, bt = 0;
+            for (int mi = -sidePixels; mi <= sidePixels; mi++) {
+                int mindex = Math.min(Math.max(mi + index, 0),
+                        mSource.length - 1);
+                int pixel = mSource[mindex];
+                rt += (float) ((pixel & 0x00ff0000) >> 16)
+                        / mBlurWidth;
+                gt += (float) ((pixel & 0x0000ff00) >> 8)
+                        / mBlurWidth;
+                bt += (float) ((pixel & 0x000000ff) >> 0)
+                        / mBlurWidth;
+            }
+
+            // Reassemble destination pixel.
+            int dpixel = (0xff000000) |
+                    (((int) rt) << 16) |
+                    (((int) gt) << 8) |
+                    (((int) bt) << 0);
+            mDestination[index] = dpixel;
+        }
+    }
+
+    protected static int sThreshold = 100000;
+
+    @Override
+    protected void compute() {
+        if (mLength < sThreshold) {
+            computeDirectly();
+            return;
+        }
+
+        int split = mLength / 2;
+
+        invokeAll(new ForkBlur(mSource, mStart, split, mDestination),
+                new ForkBlur(mSource, mStart + split, mLength - split, mDestination));
+    }
+
+    public static void main(String[] args) {
+        // source image pixels are in src
+        int[] src = new int[0];
+
+        // destination image pixels are in dst
+        int[] dst = new int[0];
+
+        ForkBlur fb = new ForkBlur(src, 0, src.length, dst);
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(fb);
+    }
+}
---
+++ concurrent/src/main/java/io/h2cone/concurrent/RecursiveActionSample.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2020 hehuang https://github.com/h2cone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.h2cone.concurrent;
+
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * Copy from https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/RecursiveAction.html
+ */
+public class RecursiveActionSample {
+
+    /**
+     * Here is a simple but complete ForkJoin sort that sorts a given long[] array
+     */
+    static class SortTask extends RecursiveAction {
+        final long[] array;
+        final int lo, hi;
+
+        SortTask(long[] array, int lo, int hi) {
+            this.array = array;
+            this.lo = lo;
+            this.hi = hi;
+        }
+
+        SortTask(long[] array) {
+            this(array, 0, array.length);
+        }
+
+        @Override
+        protected void compute() {
+            if (hi - lo < THRESHOLD) {
+                sortSequentially(lo, hi);
+            } else {
+                int mid = (lo + hi) >>> 1;
+                invokeAll(new SortTask(array, lo, mid), new SortTask(array, mid, hi));
+                merge(lo, mid, hi);
+            }
+        }
+
+        // implementation details follow:
+        static final int THRESHOLD = 1000;
+
+        void sortSequentially(int lo, int hi) {
+            Arrays.sort(array, lo, hi);
+        }
+
+        void merge(int lo, int mid, int hi) {
+            long[] buf = Arrays.copyOfRange(array, lo, mid);
+            for (int i = 0, j = lo, k = mid; i < buf.length; j++) {
+                array[j] = (k == hi || buf[i] < array[k]) ? buf[i++] : array[k++];
+            }
+        }
+    }
+
+    /**
+     * As a more concrete simple example, the following task increments each element of an array
+     */
+    static class IncrementTask extends RecursiveAction {
+        final long[] array;
+        final int lo, hi;
+
+        IncrementTask(long[] array, int lo, int hi) {
+            this.array = array;
+            this.lo = lo;
+            this.hi = hi;
+        }
+
+        @Override
+        protected void compute() {
+            if (hi - lo < SortTask.THRESHOLD) {
+                for (int i = lo; i < hi; ++i) {
+                    array[i]++;
+                }
+            } else {
+                int mid = (lo + hi) >>> 1;
+                invokeAll(new IncrementTask(array, lo, mid),
+                        new IncrementTask(array, mid, hi));
+            }
+        }
+    }
+
+    /**
+     * The following example illustrates some refinements and idioms
+     * that may lead to better performance: RecursiveActions need not be
+     * fully recursive, so long as they maintain the basic
+     * divide-and-conquer approach. Here is a class that sums the squares
+     * of each element of a double array, by subdividing out only the
+     * right-hand-sides of repeated divisions by two, and keeping track of
+     * them with a chain of {@code next} references. It uses a dynamic
+     * threshold based on method {@code getSurplusQueuedTaskCount}, but
+     * counterbalances potential excess partitioning by directly
+     * performing leaf actions on unstolen tasks rather than further
+     * subdividing.
+     */
+    double sumOfSquares(ForkJoinPool pool, double[] array) {
+        int n = array.length;
+        Applyer a = new Applyer(array, 0, n, null);
+        pool.invoke(a);
+        return a.result;
+    }
+
+    class Applyer extends RecursiveAction {
+        final double[] array;
+        final int lo, hi;
+        double result;
+        Applyer next; // keeps track of right-hand-side tasks
+
+        Applyer(double[] array, int lo, int hi, Applyer next) {
+            this.array = array;
+            this.lo = lo;
+            this.hi = hi;
+            this.next = next;
+        }
+
+        double atLeaf(int l, int h) {
+            double sum = 0;
+            for (int i = l; i < h; ++i) {   // perform leftmost base step
+                sum += array[i] * array[i];
+            }
+            return sum;
+        }
+
+        @Override
+        protected void compute() {
+            int l = lo;
+            int h = hi;
+            Applyer right = null;
+            while (h - l > 1 && getSurplusQueuedTaskCount() <= 3) {
+                int mid = (l + h) >>> 1;
+                right = new Applyer(array, mid, h, right);
+                right.fork();
+                h = mid;
+            }
+            double sum = atLeaf(l, h);
+            while (right != null) {
+                if (right.tryUnfork()) {    // directly calculate if not stolen
+                    sum += right.atLeaf(right.lo, right.hi);
+                } else {
+                    right.join();
+                    sum += right.result;
+                }
+                right = right.next;
+            }
+            result = sum;
+        }
+    }
+}
