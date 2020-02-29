bad757a4b1f0b1fa40528320e25d6ba7725f0e97
h2cone <tw8ape@gmail.com>
2020-02-29 11:03:46 +0800

    Add `TransactionId` model

---
+++ concurrent/src/main/java/io/h2cone/concurrent/TransactionId.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 hehuang https://github.com/h2cone
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.h2cone.concurrent;
+
+public class TransactionId {
+    private static final ThreadLocal<Long> tid = ThreadLocal.withInitial(() -> 0L);
+
+    public static Long get() {
+        return tid.get();
+    }
+
+    public static void set(Long value) {
+        tid.set(value);
+    }
+}
